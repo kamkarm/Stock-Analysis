8797fed8fd7aec9564b2261bb518aec76ece2cd4
kamkarm <mkamkar929@gmail.comls>
2019-08-10 16:29:45 -0400

    checked if data has already been dumped

--- initialize_table/load_data.py
+++ initialize_table/load_data.py
@@ -12,33 +12,38 @@ sys.path.append('../')
 import db
 
 if __name__ == '__main__':
-	#connect to API and get historical data from January 1st 2019 - current date
-	url = 'https://financialmodelingprep.com/api/v3/historical-price-full/AAPL,GOOG,FB'
-	param = {'timeseries' : '1'}
-	resp = requests.get(url = url, params = param)
-
-	#Make sure there is a proper connection to API
-	if resp.ok:
-		data = resp.json()['historicalStockList']
-		con = db.connect()
-		cur = con.cursor()
-		try:
-			#For every symbol (AAPL, GOOGL, FB)
-			for item in data:
-				symbol = item['symbol']
-				rows = item['historical']
-				#For every row corresponding to a day
-				for row in rows:
-					my_data = [symbol] + [row[field] for field in row]
-					#Only insert first seven elements
-					cur.execute("""
-						INSERT INTO stocks VALUES 
-				    	(%s, %s, %s, %s, %s, %s, %s)""", tuple(my_data[:7]))
-			con.commit()
-		except psycopg2.DatabaseError as error:
-			print(error)
-		finally:
-			if con is not None:
-				con.close()
+	con = db.connect()
+	cur = con.cursor()
+	#Check if data has already been loading to db
+	cur.execute('SELECT EXISTS (SELECT 1 FROM stocks)')
+	exists = cur.fetchone()[0]
+	if not exists:
+		#Connect to API and get historical data from January 1st 2019 - current date
+		url = 'https://financialmodelingprep.com/api/v3/historical-price-full/AAPL,GOOG,FB'
+		param = {'from': '2019-01-01'}
+		resp = requests.get(url = url, params = param)
+		#Make sure there is a proper connection to API
+		if resp.ok:
+			data = resp.json()['historicalStockList']
+			try:
+				#For every symbol (AAPL, GOOGL, FB)
+				for item in data:
+					symbol = item['symbol']
+					rows = item['historical']
+					#For every row corresponding to a day
+					for row in rows:
+						my_data = [symbol] + [row[field] for field in row]
+						#Only insert first seven elements
+						cur.execute("""
+							INSERT INTO stocks VALUES 
+					    	(%s, %s, %s, %s, %s, %s, %s)""", tuple(my_data[:7]))
+				con.commit()
+			except psycopg2.DatabaseError as error:
+				print(error)
+			finally:
+				if con is not None:
+					con.close()
+		else:
+			print('Connection to API Failed')
 	else:
-		print('Connection to API Failed')
+		print('Table already initialized')
