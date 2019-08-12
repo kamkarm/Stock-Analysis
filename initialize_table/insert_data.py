6b5157f9573c50e5fa4a7def33883c3f385a945a
kamkarm <mkamkar929@gmail.comls>
2019-08-12 14:53:19 -0400

    organized functions

--- initialize_table/insert_data.py
+++ initialize_table/insert_data.py
@@ -1,39 +1,86 @@
 """
-Inserts data into AWS rds database
-
-Input: Json data from Financial Modeling Prep API
+SQL query functions to create stocks table and populate it
 """
 
 import psycopg2
-import sys
-sys.path.append('../')
-import db
 
-def insert(data):
+def create_table(cur):
 
-	con = db.connect()
-	cur = con.cursor()
-	#Check if data has already been loading to db
-	cur.execute('SELECT EXISTS (SELECT 1 FROM stocks)')
-	table = cur.fetchone()[0]
-	if not table:
-		try:
-			#For every symbol (AAPL, GOOGL, FB)
-			for item in data:
-				symbol = item['symbol']
-				rows = item['historical']
-				#For every row corresponding to a day
-				for row in rows:
-					my_data = [symbol] + [row[field] for field in row]
-					#Only insert first seven elements
-					cur.execute("""
-						INSERT INTO stocks VALUES 
-				    	(%s, %s, %s, %s, %s, %s, %s)""", tuple(my_data[:7]))
-			con.commit()
-		except psycopg2.DatabaseError as error:
-			print(error)
-		finally:
-			if con is not None:
-				con.close()
-	else:
-		print('Table already initialized')
+	""" 
+	Create table stocks if it doesn't already exist (create_table)
+
+	Input: Psycopg2 cursor
+	"""
+
+	cur.execute("""
+		CREATE TABLE IF NOT EXISTS stocks (
+		symbol              TEXT,
+	    open_day            DATE,
+		opened              REAL,
+		high                REAL,
+		low                 REAL,
+		closed              REAL,
+		volume              BIGINT,
+		PRIMARY KEY (symbol, open_day)
+							)
+	""")
+
+def insert(data, cur):
+
+	""" 
+	Insert data in Json format into the stocks table (insert)
+
+	Inputs:
+
+	data: Json from fetch_data.fetch_2019_data()
+	cur: Psycopg2 cursor
+	"""
+
+	#For every symbol (AAPL, GOOGL, FB)
+	for item in data:
+		symbol = item['symbol']
+		rows = item['historical']
+		#For every row corresponding to a day
+		for row in rows:
+			my_data = [symbol] + [row[field] for field in row]
+			#Only insert first seven elements
+			cur.execute("""
+				INSERT INTO stocks VALUES 
+		    	(%s, %s, %s, %s, %s, %s, %s)""", tuple(my_data[:7]))
+
+def percent_change_column(cur):
+
+	"""
+	Add and populate column percent_change to stock table
+
+	This function will first add a column to the stocks table,
+	then replace default (NULL) values with values calculated
+	from table.
+
+	Percent_change is caculated by:
+	(row closing value) - (previous day's closing value) / row closing value * 100
+
+	Input: Psycopg2 cursor
+	"""
+	
+	cur.execute("""
+		ALTER TABLE stocks 
+		ADD COLUMN percent_change REAL
+				""")
+
+	cur.execute("""
+		UPDATE stocks
+		SET percent_change = sub.change
+		FROM (
+			SELECT 
+				symbol, 
+		   		open_day, 
+		   		(closed - LAG(closed, 1) OVER (
+											PARTITION BY symbol 
+											ORDER BY open_day
+											)) / closed * 100 AS change
+			FROM stocks) AS sub
+		WHERE 
+			stocks.symbol = sub.symbol 
+			AND stocks.open_day = sub.open_day
+				""")
--- initialize_table/load_data.py
+++ initialize_table/load_data.py
@@ -2,13 +2,47 @@
 Data dump into PostgreSQL table
 
 This script will load data from Financial Modeling Prep API from 1/02/2019 
-to current date and dump the data into an existing AWS PostgreSQL table. 
+to current date, dump the data into an existing AWS PostgreSQL table, and
+add a percent_change column (calculated from the data itself).
 """
 
+from psycopg2 import DatabaseError
+import sys
+sys.path.append('../')
+import db
 from fetch_data import fetch_2019_data
-from insert_data import insert
+import insert_data
 
 if __name__ == '__main__':
-	data = fetch_2019_data()
-	if data:
-		insert(data)
+	try:
+		con = db.connect()
+		cur = con.cursor()
+		#Create table if not exists
+		insert_data.create_table(cur)
+	except DatabaseError as error:
+		print(error)
+	else:
+		#Check if there are already rows in table
+		#If there are already rows, do nothing
+		cur.execute('SELECT EXISTS (SELECT 1 FROM stocks)')
+		rows = cur.fetchone()[0]
+		if not rows:
+			#Fetch data from API
+			data = fetch_2019_data()
+			#If data was retreived
+			if data:
+				try:
+					#Dump data into table and add percent_change column
+					#to the table
+					insert_data.insert(data, cur)
+					insert_data.percent_change_column(cur)
+					print('Data inserted to table')
+					con.commit()
+				except DatabaseError as error:
+					print(error)
+					print('There were no commits')
+		else:
+			print('Table already initialized')
+	finally:
+		if con is not None:
+			con.close()
