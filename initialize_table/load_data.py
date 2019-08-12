6201e2f4b622d9e966c04a02a83ee39f65a39688
kamkarm <mkamkar929@gmail.comls>
2019-08-12 02:23:22 -0400

    update function call name

--- initialize_table/load_data.py
+++ initialize_table/load_data.py
@@ -5,10 +5,10 @@ This script will load data from Financial Modeling Prep API from 1/02/2019
 to current date and dump the data into an existing AWS PostgreSQL table. 
 """
 
-from fetch_data import load_2019_data
+from fetch_data import fetch_2019_data
 from insert_data import insert
 
 if __name__ == '__main__':
-	data = load_2019_data()
+	data = fetch_2019_data()
 	if data:
 		insert(data)
